feaaa03256165ce1e4ecd0e83966fa41c592b735
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2019-11-29 17:41:59 +0200

    added tast-04

---
+++ lesson-23_if_statement/js/task-04.js
@@ -0,0 +1,20 @@
+// Запросить у пользователя год и проверить, високосный он или нет. 
+// Високосный год либо кратен 400, либо кратен 4 и при этом не кратен 100.
+
+let getYear = Number(prompt("task-04: Write a year: "));
+let result;
+
+if(getYear % 4 !== 0 ) {
+    result = "The year isn't leap";
+} else if (getYear % 100 === 0) {
+    if (gerYear % 400 === 0) {
+        result = "This is Leap-Year";
+    } else {
+        result = "The year isn't leap";
+    }
+
+} else {
+    result = "This is Leap-Year";
+}
+
+alert(result);
