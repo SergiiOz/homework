79161aa9d4ba5f4f8b1d9af453a3fe2b96069081
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2019-11-29 23:18:53 +0200

    added task-07

--- lesson-23_if_statement/js/task-07.js
+++ lesson-23_if_statement/js/task-07.js
@@ -1,2 +1,23 @@
 // Запросить у пользователя сумму покупки и вывести сумму к оплате со скидкой:
 // от 200 до 300 – скидка будет 3%, от 300 до 500 – 5%, от 500 и выше – 7%.
+
+let summPurchase = Number(prompt("Task-7: Write summ your purchase: "));
+let discountBronze = 3;
+let discountSilver = 5;
+let discountGold = 7;
+let totalToPay;
+
+if (summPurchase >= 200 && summPurchase < 300) {
+    totalToPay = summPurchase / 100 * (100 - discountBronze);
+
+} else if (summPurchase >= 300 && summPurchase < 500) {
+    totalToPay = summPurchase / 100 * (100 - discountSilver);
+
+} else if (summPurchase >= 500) {
+    totalToPay = summPurchase / 100 * (100 - discountGold);
+
+} else {
+    alert("You don't have discount");
+}
+
+alert("Total summ to pay equal: " + totalToPay + " usd");
