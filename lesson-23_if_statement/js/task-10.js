ed62a92ef43ecb2388b5e6f88a41fcb19e88024c
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2019-12-01 22:32:35 +0200

    added: check perion day 1-31 and check perion month 1-12

--- lesson-23_if_statement/js/task-10.js
+++ lesson-23_if_statement/js/task-10.js
@@ -2,8 +2,8 @@
 // год, а также високосный год.
 
 
-let dayOfBirth = Number(prompt('Write, the DAY of birth'));
-let monthOfBirth = Number(prompt('Write, the MONTH of birth'));
+let dayOfBirth = Number(prompt('Write, the DAY of birth, from 1 to 31'));
+let monthOfBirth = Number(prompt('Write, the MONTH of birth, form 1 to 12'));
 let yearOfBirth = Number(prompt('Write, the YEAR of birth'));
 let outputDay;
 let outputMonth;
@@ -29,78 +29,107 @@ if (yearOfBirth % 4 !== 0) {
 }
 
 
-// OUTPUT DAY  and  OUTPUT MONTH
-if (dayOfBirth > 0 && dayOfBirth <= 27) {
-    outputDay = dayOfBirth + 1;
-    outputMonth = monthOfBirth;
-    console.log(outputMonth);
 
+// OUTPUT DAY  and  OUTPUT MONTH
+//Check period between 1 and 31 
+if (dayOfBirth <= 0 || dayOfBirth > 31) {
+
+    if (monthOfBirth <= 0 || monthOfBirth > 12) {
+        alert("Error! write DAY from 1 to 31 and write MONTH from 1 to 12 ");
+
+    } else {
+        alert("Error! write DAY from 1 to 31");
+    }
+
+    // Day 1 - 27 
+} else if (dayOfBirth > 0 && dayOfBirth <= 27) {
+
+    if (monthOfBirth <= 0 || monthOfBirth > 12) {
+        alert("Error! write MONTH from 1 to 12");
+
+    } else {
+        outputDay = dayOfBirth + 1;
+        outputMonth = monthOfBirth;
+    }
+
+    // Day 28
 } else if (dayOfBirth === 28) {
 
-    if (monthOfBirth === 2 && yearLeap === true) {
+    if (monthOfBirth <= 0 || monthOfBirth > 12) {
+        alert("Error! write MONTH from 1 to 12");
+
+    } else if (monthOfBirth === 2 && yearLeap === true) {
         outputDay = dayOfBirth + 1;
         outputMonth = monthOfBirth;
-        console.log(outputMonth);
 
     } else if (monthOfBirth === 2 && yearLeap === false) {
         outputDay = 1;
         outputMonth = monthOfBirth + 1;
-        console.log(outputMonth);
 
     } else {
         outputDay = dayOfBirth + 1;
         outputMonth = monthOfBirth;
-        console.log(outputMonth);
+
     }
 
+    // Day 29
 } else if (dayOfBirth === 29) {
 
-    if (monthOfBirth === 2 && yearLeap === true) {
-        outputDay = 1;
-        outputMonth = 1;
-        console.log(outputMonth);
+    if (monthOfBirth <= 0 || monthOfBirth > 12) {
+        alert("Error! write MONTH from 1 to 12");
 
-    } else if (monthOfBirth === 2 && yearLeap === false) {
-        outputMonth = "You wrong, entered year is not a leap year";
-        console.log("You wrong, entered year is not a leap year");
-
-    } else {
-        outputDay = dayOfBirth + 1;
-        outputMonth = monthOfBirth;
-        console.log(outputMonth);
-    }
-
-} else if (dayOfBirth === 30) {
-
-    if (monthOfBirth === 4 || monthOfBirth === 6 || monthOfBirth === 9 || monthOfBirth === 11) {
-        outputDay = 1;
-        outputMonth = 1;
-        console.log(outputMonth);
-    } else {
-        outputDay = dayOfBirth + 1;
-        outputMonth = monthOfBirth;
-        console.log(outputMonth);
-    }
-
-} else if (dayOfBirth === 31) {
-
-    if (monthOfBirth === 1 || monthOfBirth === 3 || monthOfBirth === 5 || monthOfBirth === 7 || monthOfBirth === 8 || monthOfBirth === 10) {
+    } else if (monthOfBirth === 2 && yearLeap === true) {
         outputDay = 1;
         outputMonth = monthOfBirth + 1;
-        console.log(outputMonth);
 
-    } else if (monthOfBirth === 12) {
-        outputMonth = 1;
-        console.log(outputMonth);
+    } else if (monthOfBirth === 2 && yearLeap === false) {
+        outputMonth = "Error! Entered year is not a leap year. This month doesn't have 29 days.";
 
     } else {
-        outputDay = 1;
-        outputMonth = 1;
-        console.log(outputMonth);
+        outputDay = dayOfBirth + 1;
+        outputMonth = monthOfBirth;
     }
 
+    // Day 30
+} else if (dayOfBirth === 30) {
+
+    if (monthOfBirth <= 0 || monthOfBirth > 12) {
+        alert("Error! write MONTH from 1 to 12");
+
+    } else if (monthOfBirth === 4 || monthOfBirth === 6 || monthOfBirth === 9 || monthOfBirth === 11) {
+        outputDay = 1;
+        outputMonth = monthOfBirth + 1;
+
+    } else if (monthOfBirth === 2) {
+        outputMonth = "Error! This month doesn't have 30 days";
+
+    } else {
+        outputDay = dayOfBirth + 1;
+        outputMonth = monthOfBirth;
+    }
+
+    // Day 31
+} else if (dayOfBirth === 31) {
+
+    if (monthOfBirth <= 0 || monthOfBirth > 12) {
+        alert("Error! write MONTH from 1 to 12");
+
+    } else if (monthOfBirth === 1 || monthOfBirth === 3 || monthOfBirth === 5 || monthOfBirth === 7 || monthOfBirth === 8 || monthOfBirth === 10) {
+        outputDay = 1;
+        outputMonth = monthOfBirth + 1;
+
+    } else if (monthOfBirth === 12) {
+        outputDay = 1;
+        outputMonth = 1;
+
+    } else {
+        outputMonth = "Error! This month doesn't have 31 days";
+
+    }
+
+    // ERROR
 } else {
-    console.log("error!");
+    alert("Error!");
 }
 
 
@@ -108,13 +137,12 @@ if (dayOfBirth > 0 && dayOfBirth <= 27) {
 //OUTPUT YEAR
 if (dayOfBirth === 31 && monthOfBirth === 12) {
     outputYear = yearOfBirth + 1;
-    console.log(outputYear);
+
 } else {
     outputYear = yearOfBirth;
-    console.log(outputYear);
 }
 
+// OUTPUT
+alert(`Your birthday: ${dayOfBirth} : ${monthOfBirth} : ${yearOfBirth}
+    Next date after your birthday: ${outputDay} : ${outputMonth} : ${outputYear}`);
 
-
-console.log(`Your birthday: ${dayOfBirth} : ${monthOfBirth} : ${yearOfBirth}`);
-console.log(`Next date after your birthday: ${outputDay} : ${outputMonth} : ${outputYear}`);
