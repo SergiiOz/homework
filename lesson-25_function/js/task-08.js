e4dc0fdfac543ae01ef22a819537b2f7a05cd57c
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2019-12-05 23:28:50 +0200

    added task-08: convert hours, minutes and seconds to seconds

---
+++ lesson-25_function/js/task-08.js
@@ -0,0 +1,32 @@
+
+// Написать функцию, которая принимает часы, минуты и секунды 
+// и возвращает это время в секундах.
+
+
+//convert minutes to seconds
+function convertMinuteToSeconds(hour) {
+    result = hour * 60;
+
+    // console.log(result);
+    return result;
+}
+
+//convert hours to minutes
+function convertHourToMinutes(min) {
+    result = min * 60;
+
+    // console.log(result);
+    return result;
+}
+
+function convertAllToSeconds(hours, minutes, seconds) {
+
+    result = convertMinuteToSeconds(convertHourToMinutes(hours)) + convertHourToMinutes(minutes) + seconds;
+
+    console.log(result);
+    return result;
+}
+
+//run function
+
+convertAllToSeconds(10, 5, 30); //36330
