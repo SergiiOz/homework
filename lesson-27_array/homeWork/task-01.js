6ce4bbd00b9c8cbc7e5b8344905590a3138736a4
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2019-12-16 00:57:19 +0200

    added sort function

--- lesson-27_array/homeWork/task-01.js
+++ lesson-27_array/homeWork/task-01.js
@@ -17,9 +17,28 @@ let shoppingList = [
     },
 ];
 
-function displayArray(arr) {
+//sort array: first not bought item, second bought
+function sortListFirstNotBought(arr) {
+    let newArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].bought === false) {
+            newArray.unshift(arr[i]);
+        } else {
+            newArray.push(arr[i]);
+        }
+    }
+
+    return newArray;
+}
+
+//display in console all list 
+function displayList(arr) {
+
     for (let i = 0; i < arr.length; i++) {
         console.log(arr[i]);
     }
-
 }
+
+//run function
+displayList(sortListFirstNotBought(shoppingList));
