be65574575119a2f615175a5225fda30dfdc51f4
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2020-02-18 01:37:22 +0200

    fixed buttom shevchenko, added slider

--- zno/js/main.js
+++ zno/js/main.js
@@ -1,3 +1,19 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        autoplay: true,
+        speed: 3000,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        dots: true,
+        // prevArrow: $('.btn-icon-arrow__slider-prev'), //use my custom icon
+        // nextArrow: $('.btn-icon-arrow__slider-next'), //use my custom icom
+
+    });
+
+});
+
+
 // scroll to 'main' section
 let btnScroll = document.getElementsByClassName('js-btn-scroll')[0];
 let main = document.getElementsByClassName('js-main')[0];
