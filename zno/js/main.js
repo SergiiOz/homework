e14e9d828184de87cb22f636c8969881d4b85d3f
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2020-02-20 17:05:34 +0200

    cleared and added responsive properties to slider

--- zno/js/main.js
+++ zno/js/main.js
@@ -9,7 +9,34 @@ $(document).ready(function () {
         arrows: false,
         // prevArrow: $('.btn-icon-arrow__slider-prev'), //use my custom icon
         // nextArrow: $('.btn-icon-arrow__slider-next'), //use my custom icom
-
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
 
 });
